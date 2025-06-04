7d6bf4976f1e1e1f3e56799596bacbdc9c1de5db
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-04 14:50:05 -0500

    Update Main.java

--- lab_text_adventure_complete/src/Main.java
+++ lab_text_adventure_complete/src/Main.java
@@ -27,17 +27,17 @@ public class Main {
                 
                 5. The silver door\
                 
-                Which door do you choose to enter? (1,2,3) or (q to quit):\s""";
+                Which door do you choose to enter? (1,2,3):\s""";
 
         rooms [0] = "\nYou push the brass door open, revealing a small aquatic-themed library. On a side table, you see a book with a brass key being used as a bookmark. You take the key";
 
         rooms [1] = "\nYou use all your might to push open the shiny steel door. You see a room full of medieval weapons displayed on the walls and in cases and racks. You find a steel key hanging on one of the weapon racks!";
         rooms [2] = "\nYou attempt to open the golden door, but it won't open. You notice four keyholes the other doors do not have...";
         rooms [3] = "\nYou push open the copper door and reveal a room full of piles copper currencies old and new. You search a few piles and finally find a copper key!";
-        rooms [4] = "\nThe silver door slides to the side, into the wall, and you find a large taxidermy werewolf! Around its neck is a silver key on a cord. You carefully claim the key."
+        rooms [4] = "\nThe silver door slides to the side, into the wall, and you find a large taxidermy werewolf! Around its neck is a silver key on a cord. You carefully claim the key.";
 
         seenRooms[0] = "\nYou go through the brass door again. You find nothing else in the library.";
-        seenRooms[1] = "\nYou go through the steel door again. You admire the many weapons, but there aren't any more half-keys.";
+        seenRooms[1] = "\nYou go through the steel door again. You admire the many weapons, but there aren't any more keys.";
         seenRooms[2] = "\nYou insert all four keys and unlock the golden door. When you open it, you find... the exit!";
         seenRooms[3] = "\nYou go through the copper door again. Somehow the piles are now in ordered stacks. Nothing else is new.";
         seenRooms[4] = "\nYou go through the silver door again. Did the stuffed werewolf move? No... of course not. You leave quickly.";
@@ -60,10 +60,23 @@ public class Main {
                     break;
                 case 5:
                     boolean unlock = true;
-                default :
+                for (int i = 0; i < keys.length; i++) {
+                    if (keys[i] == false) {
+                        System.out.println(rooms[2]);
+                        unlock = false;
+                        break;
+                    }
+                }
+                if (unlock) {
+                    System.out.println(rooms[2]);
+                    keepLooping = false;
+                }
+                break;
+
+                default:
                     System.out.println("\nYou seem distracted by the decor of each door; try choosing again.");
             }
         }
-        System.out.println("\nThanks for playing the Three Doors!");
+        System.out.println("\nThanks for playing the Five Doors!");
     }
 }
