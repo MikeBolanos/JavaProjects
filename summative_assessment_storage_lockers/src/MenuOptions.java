1c6ab4f871f8dc258924aa7887e599ff5c4ddf5b
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-07 17:04:05 -0500

    Create MenuOptions.java

---
+++ summative_assessment_storage_lockers/src/MenuOptions.java
@@ -0,0 +1,18 @@
+public enum MenuOptions {
+    RENT ("1"),
+    ACCESS("2"),
+    RELEASE("3"),
+    QUIT("4"),
+    INVALID("");
+
+
+
+    // Variable to store user input for option
+    private String ;
+
+    // Constructor
+    MenuOptions(String menuOption)
+
+    // Method for menu options
+
+}
