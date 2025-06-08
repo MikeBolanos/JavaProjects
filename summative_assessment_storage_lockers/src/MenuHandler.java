9fa83e6ce46a34b98f8a61ae7e00b57e1bd0a092
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-07 19:51:11 -0500

    Reorganizing code to look more clean

--- summative_assessment_storage_lockers/src/MenuHandler.java
+++ summative_assessment_storage_lockers/src/MenuHandler.java
@@ -1,4 +1,9 @@
 public class MenuHandler {
+    // Locker Service
+    private LockerService lockerService;
+    // boolean for loop
+    private boolean testing;
+
     public enum MenuOption {
         RENT ("1"),
         ACCESS("2"),
@@ -24,6 +29,59 @@ public class MenuHandler {
                 }
             }
             return  INVALID;
+        }
+    }
+    // Loop:
+    // Use while true (boolean) loop for program
+        while (testing) {
+        // Check if all lockers are rented
+        boolean allLockersRented = lockerService.lockersFull();
+
+        // Main menu (testing)
+        Utils.print("\n~*~*~*~ Lockdown Lockers Test ~*~*~*~");
+        // Check if all lockers are full before displaying Rent option
+        if (!allLockersRented) {
+            Utils.print("1. Rent a Locker");
+        }
+        Utils.print("2. Access a Locker");
+        Utils.print("3. Release a Locker");
+        Utils.print("4. Quit");
+
+        // Prompt for menu choice
+        String choice = Utils.prompt("Please choose an option (1, 2, 3, or 4): ");
+        MenuHandler.MenuOption option = MenuHandler.MenuOption.menuChoice(choice);
+
+        //Switch cases for Menu choices
+        switch (option) {
+
+            case RENT: // case 1: Rent a locker | Call rentLocker method
+                if (allLockersRented) {
+                    Utils.print("All lockers are rented. At least one locker must be released to rent a locker.");
+                    break;
+                }
+                Result rent = lockerService.rentLocker();
+                Utils.print(rent.getMessage());
+                break;
+
+            case ACCESS: // case 2: Access a locker | Call accessLocker method
+                String accessPin = Utils.prompt("\nTo access a locker, please enter your PIN: ");
+                Result access = lockerService.accessLocker(accessPin);
+                Utils.print(access.getMessage());
+                break;
+
+            case RELEASE: // case 3: Release a locker |
+                String releasePin = Utils.prompt("\nTo release a locker, please enter your PIN: ");
+                Result release = lockerService.handleLockerRelease(releasePin);
+                Utils.print(release.getMessage());
+                break;
+
+            case QUIT: // case 4: "q" to quit | Call print method
+                Utils.print("\nThanks for using the locker system by Lockdown Inc! Have great day!");
+                testing = false;
+                break;
+
+            default: // Invalid input choice message via print method
+                Utils.print("\nInvalid choice. Please try again.");
         }
     }
 }
