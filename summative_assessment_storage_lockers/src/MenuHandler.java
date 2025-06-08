04ba02eb2b076cb5fd606cfca099fbff0a2a5065
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-07 19:59:28 -0500

    Update MenuHandler.java

--- summative_assessment_storage_lockers/src/MenuHandler.java
+++ summative_assessment_storage_lockers/src/MenuHandler.java
@@ -11,7 +11,7 @@ public class MenuHandler {
     }
 
     public enum MenuOption {
-        RENT ("1"),
+        RENT("1"),
         ACCESS("2"),
         RELEASE("3"),
         QUIT("4"),
@@ -34,61 +34,63 @@ public class MenuHandler {
                     return option;
                 }
             }
-            return  INVALID;
+            return INVALID;
         }
     }
 
-    // Loop:
-    // Use while true (boolean) loop for program
-        while (this.testing) {
-        // Check if all lockers are rented
-        boolean allLockersRented = lockerService.lockersFull();
+    public void menuLoop() {
+        // Loop:
+        // Use while true (boolean) loop for program
+        while (testing) {
+            // Check if all lockers are rented
+            boolean allLockersRented = lockerService.lockersFull();
 
-        // Main menu (testing)
-        Utils.print("\n~*~*~*~ Lockdown Lockers Test ~*~*~*~");
-        // Check if all lockers are full before displaying Rent option
-        if (!allLockersRented) {
-            Utils.print("1. Rent a Locker");
-        }
-        Utils.print("2. Access a Locker");
-        Utils.print("3. Release a Locker");
-        Utils.print("4. Quit");
+            // Main menu (testing)
+            Utils.print("\n~*~*~*~ Lockdown Lockers Test ~*~*~*~");
+            // Check if all lockers are full before displaying Rent option
+            if (!allLockersRented) {
+                Utils.print("1. Rent a Locker");
+            }
+            Utils.print("2. Access a Locker");
+            Utils.print("3. Release a Locker");
+            Utils.print("4. Quit");
 
-        // Prompt for menu choice
-        String choice = Utils.prompt("Please choose an option (1, 2, 3, or 4): ");
-        MenuHandler.MenuOption option = MenuHandler.MenuOption.menuChoice(choice);
+            // Prompt for menu choice
+            String choice = Utils.prompt("Please choose an option (1, 2, 3, or 4): ");
+            MenuHandler.MenuOption option = MenuHandler.MenuOption.menuChoice(choice);
 
-        //Switch cases for Menu choices
-        switch (option) {
+            //Switch cases for Menu choices
+            switch (option) {
 
-            case RENT: // case 1: Rent a locker | Call rentLocker method
-                if (allLockersRented) {
-                    Utils.print("All lockers are rented. At least one locker must be released to rent a locker.");
+                case RENT: // case 1: Rent a locker | Call rentLocker method
+                    if (allLockersRented) {
+                        Utils.print("All lockers are rented. At least one locker must be released to rent a locker.");
+                        break;
+                    }
+                    Result rent = lockerService.rentLocker();
+                    Utils.print(rent.getMessage());
                     break;
-                }
-                Result rent = lockerService.rentLocker();
-                Utils.print(rent.getMessage());
-                break;
 
-            case ACCESS: // case 2: Access a locker | Call accessLocker method
-                String accessPin = Utils.prompt("\nTo access a locker, please enter your PIN: ");
-                Result access = lockerService.accessLocker(accessPin);
-                Utils.print(access.getMessage());
-                break;
+                case ACCESS: // case 2: Access a locker | Call accessLocker method
+                    String accessPin = Utils.prompt("\nTo access a locker, please enter your PIN: ");
+                    Result access = lockerService.accessLocker(accessPin);
+                    Utils.print(access.getMessage());
+                    break;
 
-            case RELEASE: // case 3: Release a locker |
-                String releasePin = Utils.prompt("\nTo release a locker, please enter your PIN: ");
-                Result release = lockerService.handleLockerRelease(releasePin);
-                Utils.print(release.getMessage());
-                break;
+                case RELEASE: // case 3: Release a locker |
+                    String releasePin = Utils.prompt("\nTo release a locker, please enter your PIN: ");
+                    Result release = lockerService.handleLockerRelease(releasePin);
+                    Utils.print(release.getMessage());
+                    break;
 
-            case QUIT: // case 4: "q" to quit | Call print method
-                Utils.print("\nThanks for using the locker system by Lockdown Inc! Have great day!");
-                testing = false;
-                break;
+                case QUIT: // case 4: "q" to quit | Call print method
+                    Utils.print("\nThanks for using the locker system by Lockdown Inc! Have great day!");
+                    testing = false;
+                    break;
 
-            default: // Invalid input choice message via print method
-                Utils.print("\nInvalid choice. Please try again.");
+                default: // Invalid input choice message via print method
+                    Utils.print("\nInvalid choice. Please try again.");
+            }
         }
     }
 }
