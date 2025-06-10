08af32762550ee48383f1c7fc3defa47a8efae57
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-09 20:54:22 -0500

    Update Main.java

--- exercise_storage_lockers_objects_and_classes/src/Main.java
+++ exercise_storage_lockers_objects_and_classes/src/Main.java
@@ -18,7 +18,23 @@ public class Main {
             System.out.println("6 Exit");
             System.out.println("Please enter the number of your choice: ");
 
-            
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1: // add locker
+                    System.out.println("Enter an ID to add locker");
+                    String newId = scanner.nextLine();
+                    service.addLocker(newId);
+                    break;
+
+                case 2: // remove locker
+                    System.out.println("Enter a locker ID to remove");
+                    String removeId = scanner.nextLine();
+                    service.removeLocker(removeId);
+                    break;
+            }
+                
 
 
         }
