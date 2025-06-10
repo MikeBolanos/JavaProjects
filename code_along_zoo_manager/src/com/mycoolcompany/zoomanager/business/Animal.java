1a23c42a0a9d59d8eb25856cd9bcb8ee3950957b
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-10 13:02:35 -0500

    code_along_zoo_manager

---
+++ code_along_zoo_manager/src/com/mycoolcompany/zoomanager/business/Animal.java
@@ -0,0 +1,32 @@
+package com.mycoolcompany.zoomanager.business;
+
+public class Animal {
+    private String name; // Name of animal species
+    private int populationCount; // Total amount of species in zoo
+    private String sound; // Sound the animal makes
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulationCount() {
+        return populationCount;
+    }
+
+    public void setPopulationCount(int populationCount) {
+        this.populationCount = populationCount;
+    }
+
+    public String getSound() {
+        return sound;
+    }
+
+    public void setSound(String sound) {
+        this.sound = sound;
+    }
+}
+
