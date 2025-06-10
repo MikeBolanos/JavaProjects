ffc23cd0164d6ee8e228b35a77feccc28b6a3958
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-09 19:51:35 -0500

    Update LockerManager.java

--- exercise_storage_lockers_objects_and_classes/src/LockerManager.java
+++ exercise_storage_lockers_objects_and_classes/src/LockerManager.java
@@ -1,15 +1,24 @@
+import java.util.ArrayList;
+
 public class LockerManager {
     // Attributes
 
     // lockers (ArrayList<Locker>): A list to store multiple Locker objects
-
+    private ArrayList<Locker> lockers;
 
     // Constructor
     // LockerManager(): Initializes list of lockers
-
+    public LockerManager() {
+        lockers = new ArrayList<>();
+    }
     // Methods
     // 1) void addLockers(String lockerId): Adds a new locker to list
-
+    public void addLockers(String lockerId) {
+        for (int i = 0; i < lockers.size(); i++) {
+            Locker currentLocker = lockers.get(i);
+            if (.equals()
+        }
+    }
     // 2) Locker getLocker
 
 
