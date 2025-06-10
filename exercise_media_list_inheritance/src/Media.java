34755a210162322fd02d925d2395a4b7ed2e25fb
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-10 16:19:25 -0500

    media list exercise

---
+++ exercise_media_list_inheritance/src/Media.java
@@ -0,0 +1,26 @@
+public abstract class Media {
+    // Fields
+    protected String name; // Media name
+
+    // Methods
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name (void because it doesn't return anything)
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Abstract method to be overridden by subclasses
+    abstract void play();
+
+
+    // Abstract method to be overridden by subclasses
+    abstract String getDescription();
+
+
+
+}
---
+++ exercise_media_list_inheritance/src/Video.java
@@ -0,0 +1,25 @@
+public class Video {
+
+    // Fields
+    int duration; // duration in minutes
+    String resolution; // video resolution (e.g. 720p, 1080p, 4K
+
+    // Getters
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    // Setters
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+}
