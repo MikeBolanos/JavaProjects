3013d4fd22cd8d174cd4e79426ae391e454f364d
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-10 13:42:48 -0500

    Update Zoo.java

--- code_along_zoo_manager/src/com/mycoolcompany/zoomanager/business/Zoo.java
+++ code_along_zoo_manager/src/com/mycoolcompany/zoomanager/business/Zoo.java
@@ -30,8 +30,7 @@ public class Zoo {
     public void printPopulationReport() {
         System.out.println("Population Report: " + name);
 
-        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
-        System.out.println();
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n");
         for (int i = 0; i < population.length; i++) {
             Animal a = population[i];
             System.out.println(a.getName() + ": " + a.getPopulationCount());
@@ -39,8 +38,13 @@ public class Zoo {
         System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
     }
     public void printAnimalSounds() {
-            
+            System.out.println("The Sounds of: " + name);
+        System.out.println("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~\n");
+        for (int i = 0; i < population.length; i++){
+            Animal a = population[i];
+            System.out.println(a.getSound());
         }
+    }
 
 
     // Animal Sounds Report: Print a header, followed by all the sounds we'll hear in the zoo
