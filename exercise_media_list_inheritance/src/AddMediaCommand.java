fc1b7f3d6da196f222ce63581f0cafba14945702
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-11 14:13:24 -0500

    Update AddMediaCommand.java

--- exercise_media_list_inheritance/src/AddMediaCommand.java
+++ exercise_media_list_inheritance/src/AddMediaCommand.java
@@ -1,6 +1,6 @@
 public class AddMediaCommand {
-    // handles adding new media, be sure to first prompt for the media type and
-    //collect all the data specific to that type (Videos will have different inputs than audio or books)
+    // handles adding new media, be sure to first prompt for the media type
+    //collect all the data specific to that type
 
     // Fields
     private MediaService service;
@@ -13,6 +13,30 @@ public class AddMediaCommand {
         this.utils = utils;
     }
 
+    // Methods
+
+    // execute
+    public void execute() {
+        utils.displayMessage("\n~*~*~Add New Media~*~*~");
+        utils.displayMessage("1. Video");
+        utils.displayMessage("2. Audio");
+        utils.displayMessage("3. Book");
+        utils.displayMessage("4. Image");
+
+        int choice = utils.getInt("\nSelect a media type by number: ");
+
+        switch (choice) {  // cases will call add methods for each media type
+            case 1 -> addVideo();
+            case 2 -> addAudio();
+            case 3 -> addBook();
+            case 4 -> addimage();
+
+        }
+
+
+
+
+    }
 
 
 
