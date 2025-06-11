b0f3aa034c958925e103c1955b0b26d236af8878
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-11 15:51:55 -0500

    Update MediaService.java

--- exercise_media_list_inheritance/src/MediaService.java
+++ exercise_media_list_inheritance/src/MediaService.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.stylesheets.MediaList;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,10 +20,20 @@ public class MediaService {
     }
 
     // removeMedia(String name) - removes media by name, returns boolean success
-    public boolean removeMedia(String name) { // Need to build findMediaByName to make this work?
-        
+    public boolean removeMedia(String name) {
+        // Need to build findMediaByName to make this work?
 
+
+    }
+
+    // findMediaByName(String name) - finds and returns Media by name
+    public Media findMediaByName(String name) {
+        for (int i = 0; i < mediaList.size(); i++) {
+            Media media = mediaList.get(i);       // ***** Use the index? *****
+            if (media.getName().equalsIgnoreCase(name)) { // returns match if found, ignoring case
+                return media;
+            }
         }
+        return null; // returns nothing if no match found.
     }
 }
-
