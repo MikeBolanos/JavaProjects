ac3d3e118d18492724cf38fea232717af51d8978
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-11 18:57:19 -0500

    Update Main.java

--- exercise_media_list_inheritance/src/Main.java
+++ exercise_media_list_inheritance/src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        // Create instance for MediaService and TerminalUtils
+        MediaService service = new MediaService();
+        TerminalUtils utils = new TerminalUtils();
 
+        //  New AddMediaCommand with above instances
+        AddMediaCommand addMedia = new AddMediaCommand(service, utils);
+
+        // Calling addMedia execute method to test
+        addMedia.execute();
+        
     }
 }
