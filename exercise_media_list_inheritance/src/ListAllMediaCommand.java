600f4323c5ecfd87ae177368787b348a131bb1ff
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-11 20:03:42 -0500

    Update ListAllMediaCommand.java

--- exercise_media_list_inheritance/src/ListAllMediaCommand.java
+++ exercise_media_list_inheritance/src/ListAllMediaCommand.java
@@ -1,2 +1,14 @@
 public class ListAllMediaCommand {
+    // ListAllMediaCommand - handles displaying all media
+
+    // Fields
+    private MediaService service;
+    private TerminalUtils utils;
+
+    // Constructor
+    public ListAllMediaCommand(MediaService service, TerminalUtils utils) {
+        this.service = service;
+        this.utils = utils;
+        
+    }
 }
