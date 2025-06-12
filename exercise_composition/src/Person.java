3f0fc5072ef07371351e370c1f08c21c9466cc68
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-12 13:51:13 -0500

    Building Person and Main class

---
+++ exercise_composition/src/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    // Fields
+    private String firstName;
+    private String lastName;
+    private String primarySpecialty;
+
+    // Constructor
+    public Person(String firstName, String lastName, String primarySpecialty) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.primarySpecialty = primarySpecialty;
+    }
+
+    // Getters
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPrimarySpecialty() {
+        return primarySpecialty;
+    }
+
+    
+}
