a5b48a8968f58b7ac828b5a9279973ca37fd2f56
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-12 15:12:17 -0500

    Update Main.java

--- exercise_composition/src/Main.java
+++ exercise_composition/src/Main.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        // 1. Enter the name of a new Artifact.
+        System.out.println("Enter the name of the artifact: ");
+        String artifactName = scanner.nextLine();
+
+        // 2. Enter the year of its discovery.
+        
     }
 }
