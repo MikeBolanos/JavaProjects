0eee0ea7fbeb229cb966750e88e9203afbab7ad6
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-13 13:55:38 -0500

    Update GameApp.java

--- lab_oop_video_game_characters/src/GameApp.java
+++ lab_oop_video_game_characters/src/GameApp.java
@@ -3,9 +3,11 @@ import java.util.List;
 
 public class GameApp {
     public static void main(String[] args) {
-        List<Character> characters = new ArrayList<>();
+        List<Character> characters = new ArrayList<>(); // Create new ArrayList of characters and add the
         characters.add(new Warrior("Conan", 98, 20, "greatsword"));
         characters.add(new Mage("Merlin", 70, 25, "lightning bolt" ));
         characters.add(new Archer("Robin", 63, 18, "longbow"));
     }
+    // Loop though list and call their attack method.
+    for ()
 }
