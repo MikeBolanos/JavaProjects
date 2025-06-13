8bc3964510287e3f6789003cc3274809eb604d74
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-13 12:17:25 -0500

    Update Character.java

--- lab_oop_video_game_characters/src/Character.java
+++ lab_oop_video_game_characters/src/Character.java
@@ -4,4 +4,14 @@ public class Character {
     private String name;
     private int health;
     private int attackPower;
+
+    // Constructor
+    public Character( String name, int health, int attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+
+    // Method for attack action
+    
 }
