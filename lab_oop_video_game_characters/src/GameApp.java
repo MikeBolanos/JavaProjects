db037158411025c4e426d7341b5f49d1d5e962a8
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-13 13:50:41 -0500

    Update GameApp.java

--- lab_oop_video_game_characters/src/GameApp.java
+++ lab_oop_video_game_characters/src/GameApp.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameApp {
-    
+    public static void main(String[] args) {
+        List<Character> characters = new ArrayList<>();
+        characters.add(new Warrior("Conan", 98, 20, "greatsword"));
+        characters.add(new Mage("Merlin", 70, 25, "lightning bolt" ));
+        characters.add(new Archer("Robin", 63, 18, "longbow"));
+    }
 }
