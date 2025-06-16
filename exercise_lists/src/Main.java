ac6c8827292c36f2bca82ae1f271ca6b6fc53aa2
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-16 15:08:47 -0500

    Create Main.java

---
+++ exercise_lists/src/Main.java
@@ -0,0 +1,19 @@
+import java.lang.classfile.constantpool.ModuleEntry;
+import java.security.interfaces.RSAMultiPrimePrivateCrtKey;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        // Declare an ArrayList<String> named students
+        ArrayList<String> students = new ArrayList<String>();
+        // Add at least five student names to the list using .add()
+        students.add("Larry");
+        students.add("Curly");
+        students.add("Moe");
+        students.add("Mike");
+        students.add("Heather");
+
+        // Use .get() to print the third student in the list
+        students.get()
+    }
+}
