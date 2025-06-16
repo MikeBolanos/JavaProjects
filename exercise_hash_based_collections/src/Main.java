999f4a9b2a8ca5731937d73cdf01a15a9cd7bb1e
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-16 16:06:11 -0500

    Created Hashmap exercise Main,java file
    Imported Map, began HashMap of months

---
+++ exercise_hash_based_collections/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        //Create a HashMap with keys of the month numbers of the year and values of the String names
+        Map<Integer, String> monthMap = new HashMap<>();
+        monthMap.put(1, "January");
+        monthMap.put(2, "February");
+        
+    }
+}
