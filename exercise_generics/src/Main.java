0019336e87e82493896ef198255a08886818b953
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-16 11:52:54 -0500

    Updated Main.java
    Set Box items and a way to get the values.

--- exercise_generics/src/Main.java
+++ exercise_generics/src/Main.java
@@ -1,6 +1,23 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // Create an instance of Box that stores an Integer
-        
+        Box<Integer> integerBox = new Box<>();
+        integerBox.setT(100);
+        System.out.println("\nNumber Box contains: " + integerBox.getT());
+
+        // Create an instance of Box that stores a String
+
+        Box<String> stringBox = new Box<>();
+        stringBox.setT("Hello, Generics!");
+        System.out.println("Message Box contains: " + stringBox.getT());
+
+
+        // ArrayList
+      
+
     }
 }
+
+
