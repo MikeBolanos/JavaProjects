68094483bf46c8b3411ba3202cbc8b7df026cfa7
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-16 16:41:04 -0500

    Updated Main.java

--- exercise_hash_based_collections/src/Main.java
+++ exercise_hash_based_collections/src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -7,6 +9,24 @@ public class Main {
         Map<Integer, String> monthMap = new HashMap<>();
         monthMap.put(1, "January");
         monthMap.put(2, "February");
-        
+        monthMap.put(3, "March");
+        monthMap.put(4, "April");
+        monthMap.put(5, "May");
+        monthMap.put(6, "June");
+        monthMap.put(7, "July");
+        monthMap.put(8, "August");
+        monthMap.put(9, "September");
+        monthMap.put(10, "October");
+        monthMap.put(11, "November");
+        monthMap.put(12, "December");
+
+        // Use the keySet method in a for loop to print all the keys and their values to the console.
+        for (Integer key : monthMap.keySet()) {
+            System.out.println("Month #" + key + " is " + monthMap.get(key));
+        }
+
+        // Create a HashMap<String, List<String>> and load it with a card deck
+        Map<String, List<String>> cardDeck = new HashMap<>();
+        cardDeck.put("Hearts", new ArrayList<>()
     }
 }
