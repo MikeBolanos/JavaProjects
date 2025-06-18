c80eae5b5380fa9bdad5e9a1a724e62f1050c9d4
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-18 10:50:55 -0500

    Added Incendio class
    Added class, and updated another

--- exercise-spellbook-terminal/src/main/java/wiz/hogwarts/spells/ExitSpell.java
+++ exercise-spellbook-terminal/src/main/java/wiz/hogwarts/spells/ExitSpell.java
@@ -1,12 +1,11 @@
 package wiz.hogwarts.spells;
 
 public class ExitSpell implements Spell {
-    @Override
     public void cast() {
         System.out.println("A portal opens beneath your feet. Goodbye, wizard!");
         System.exit(0);
     }
-    @Override
+
     public String getIncantation() {
         return "quit";
     }
---
+++ exercise-spellbook-terminal/src/main/java/wiz/hogwarts/spells/Incendio.java
@@ -0,0 +1,11 @@
+package wiz.hogwarts.spells;
+
+public class Incendio implements Spell {
+    public void cast(){
+        System.out.println("You conjure fire to ignite your target in a searing blaze!");
+    }
+
+    public String getIncantation() {
+        return "Incendio";
+    }
+}
