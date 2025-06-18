548ecdb2bdb848cea6f98b0348886172c141075e
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-18 11:47:25 -0500

    Update App.java

--- exercise-spellbook-terminal/src/main/java/wiz/hogwarts/App.java
+++ exercise-spellbook-terminal/src/main/java/wiz/hogwarts/App.java
@@ -8,6 +8,11 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         SpellBook spellbook = new SpellBook();
 
+        do {
+            System.out.println("Recite a spell: ");
+            String incantation = scanner.nextLine();
+            spellbook.tryIncantation(incantation);
+        }
 
     }
 }
