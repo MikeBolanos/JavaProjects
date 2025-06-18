995ff790f4fd67123f833da7b7c19cb8b6684de7
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-18 11:05:11 -0500

    Create Protego.java
    And coded the cast and getIncantation methods from Spell interface

---
+++ exercise-spellbook-terminal/src/main/java/wiz/hogwarts/spells/Protego.java
@@ -0,0 +1,12 @@
+package wiz.hogwarts.spells;
+
+public class Protego implements Spell {
+
+    public void cast() {
+    System.out.println("You conjure a shimmering, translucent shield to protect you from other spells.");
+    }
+
+    public String getIncantation () {
+        return "Protego";
+    }
+}
