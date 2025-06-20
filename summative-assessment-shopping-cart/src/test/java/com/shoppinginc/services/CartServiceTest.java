cc1bf5b0e0de1e1b7a2fe8c32e8c0454242196fa
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-20 16:27:19 -0500

    Update CartServiceTest.java
    Testing addItem method in cartService class.

--- summative-assessment-shopping-cart/src/test/java/com/shoppinginc/services/CartServiceTest.java
+++ summative-assessment-shopping-cart/src/test/java/com/shoppinginc/services/CartServiceTest.java
@@ -1,4 +1,17 @@
 package com.shoppinginc.services;
+import com.shoppinginc.models.*;
+import org.junit.jupiter.api.Test;
 
 public class CartServiceTest {
+    @Test
+    public void testAddItem() {
+        // Arrange
+        Catalog catalog = new Catalog();
+        CartService cartService = new CartService(catalog);
+        Item milk = catalog.getItemByName("Milk");
+
+        // Act
+        
+        // Assert
+    }
 }
