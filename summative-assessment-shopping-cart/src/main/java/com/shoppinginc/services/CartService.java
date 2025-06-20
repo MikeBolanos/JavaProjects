afd57e715a690f13cd0f42abcb7b873af53911b6
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-20 16:11:49 -0500

    Update CartService.java
    Coding cartService class

--- summative-assessment-shopping-cart/src/main/java/com/shoppinginc/services/CartService.java
+++ summative-assessment-shopping-cart/src/main/java/com/shoppinginc/services/CartService.java
@@ -1,9 +1,37 @@
 package com.shoppinginc.services;
-
+import com.shoppinginc.models.*;
 import com.shoppinginc.interfaces.CartCommands;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CartService implements CartCommands {
-    
+    // fields
+    // Catalog of all items in store
+    private final Catalog catalog;
+
+    // Cart map
+    private final Map<String, Integer> cartItems;
+
+    // Constructor
+    public CartService(Catalog catalog) {
+        this.catalog = catalog;
+        this.cartItems = new HashMap<>();
+    }
+
+    // Add item method
+    @Override
+    public void addItem(Item item, int quantity) {
+        String name = item.getName();
+
+        int currentItems;
+
+        if (cartItems.containsKey(name)) {
+            currentItems = cartItems.get(name);
+        } else {
+            currentItems = 0;
+        }
+    }
 
 
 }
