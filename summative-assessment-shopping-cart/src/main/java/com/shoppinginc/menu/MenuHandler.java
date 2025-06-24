79af4c87b84887d8dae1af200f6e89295db29dd3
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-24 18:42:04 -0500

    Update MenuHandler.java
    coding MenuHandler

--- summative-assessment-shopping-cart/src/main/java/com/shoppinginc/menu/MenuHandler.java
+++ summative-assessment-shopping-cart/src/main/java/com/shoppinginc/menu/MenuHandler.java
@@ -1,7 +1,9 @@
 package com.shoppinginc.menu;
 
+import com.shoppinginc.commands.*;
 import com.shoppinginc.models.*;
 import com.shoppinginc.services.CartService;
+import com.shoppinginc.utils.Utils;
 
 public class MenuHandler {
     private final Catalog catalog;
@@ -13,6 +15,18 @@ public class MenuHandler {
     }
 
     public boolean handle(MenuOption option) {
-        
+        switch (option) {
+            case VIEW_CATALOG -> catalog.displayCatalog();
+
+            case ADD_ITEM -> {
+                String name = Utils.promptString("Enter an item from the Catalog: ").trim();
+                Item item = catalog.getItemByName(name);
+                if (item == null) {
+                    Utils.print("Item not found");
+                } else {
+                    int quantity = Utils.promptInt("Enter quantity: ");
+                    new AddItemCommand(cartService, item, quantity).execute();
+            }
+        }
     }
 }
