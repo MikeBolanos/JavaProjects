6df75ca5b804480738cdd494887b23c14d724adf
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-23 19:37:08 -0500

    Update Main.java
    Building main menu.

--- summative-assessment-shopping-cart/src/main/java/com/shoppinginc/app/Main.java
+++ summative-assessment-shopping-cart/src/main/java/com/shoppinginc/app/Main.java
@@ -10,7 +10,33 @@ public class Main {
         Catalog catalog = new Catalog();
         CartCommands cart = new CartService(catalog);
 
-        
+        boolean looping;
+
+        do {
+
+            // Header
+            Utils.print("\n~*~*~*~Shopping Inc Menu~*~*~*~");
+            // Display Menu
+            Utils.print("1. View Catalog");
+            Utils.print("2. Add Item to Cart");
+            Utils.print("3. Remove Item from Cart");
+            Utils.print("4. View Cart");
+            Utils.print("5. Checkout");
+            Utils.print("6. Exit");
+
+            int choice = Utils.promptInt("Enter your choice via number(1-6)");
+            // 1. View Catalog
+
+            // 2. Add Item
+
+            // 3. Remove item
+
+            // 4. View Cart
+
+            // 5. Checkout
+
+            // 6. Exit
+        }
 
 
     }
