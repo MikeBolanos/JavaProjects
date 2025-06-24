de85d2e8b473b23a272de333d8f88f9b35bf1352
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-23 19:24:19 -0500

    Update Main.java
    Building main menu.

--- summative-assessment-shopping-cart/src/main/java/com/shoppinginc/app/Main.java
+++ summative-assessment-shopping-cart/src/main/java/com/shoppinginc/app/Main.java
@@ -1,13 +1,17 @@
 package com.shoppinginc.app;
 
+import com.shoppinginc.interfaces.CartCommands;
+import com.shoppinginc.utils.Utils;
 import com.shoppinginc.models.*;
+import com.shoppinginc.services.CartService;
 
 public class Main {
     public static void main(String[] args) {
         Catalog catalog = new Catalog();
-        {
-            catalog.displayCatalog();
-        }
+        CartCommands cart = new CartService(catalog);
+
+        
+
+
     }
-}
 
