62e126e06d2b908cba11740933822c5871a67c86
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-24 18:38:24 -0500

    Update MenuHandler.java
    Coding MenuHandler class

--- summative-assessment-shopping-cart/src/main/java/com/shoppinginc/menu/MenuHandler.java
+++ summative-assessment-shopping-cart/src/main/java/com/shoppinginc/menu/MenuHandler.java
@@ -1,9 +1,18 @@
 package com.shoppinginc.menu;
 
-import com.shoppinginc.commands.*;
-import com.shoppinginc.utils.Utils;
 import com.shoppinginc.models.*;
 import com.shoppinginc.services.CartService;
 
-public class MenuService {
+public class MenuHandler {
+    private final Catalog catalog;
+    private final CartService cartService;
+
+    public MenuHandler(Catalog catalog, CartService cartService) {
+        this.catalog = catalog;
+        this.cartService = cartService;
+    }
+
+    public boolean handle(MenuOption option) {
+        
+    }
 }
