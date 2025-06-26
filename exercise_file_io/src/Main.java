8d8e2068f972177e470247219896cf5657ebf87c
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-26 11:30:43 -0500

    exercise file io
    creating file

---
+++ exercise_file_io/src/Main.java
@@ -0,0 +1,17 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+//     File file = new File("student_data.txt");
+//     try {
+//         if (file.createNewFile()) {
+//             System.out.println("student_data.txt created");
+//         } else {
+//             System.out.println("student_data.txt already exists");
+//         }
+//     } catch (IOException ex) {
+//         ex.printStackTrace();
+     }
+    }
+}
