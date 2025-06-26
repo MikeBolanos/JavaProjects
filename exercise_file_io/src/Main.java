76b435eadbf82f5bff9bf54a9e56123f7363fbe2
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-26 11:31:56 -0500

    Update Main.java
    Deleted file, created file again

--- exercise_file_io/src/Main.java
+++ exercise_file_io/src/Main.java
@@ -3,15 +3,15 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-//     File file = new File("student_data.txt");
-//     try {
-//         if (file.createNewFile()) {
-//             System.out.println("student_data.txt created");
-//         } else {
-//             System.out.println("student_data.txt already exists");
-//         }
-//     } catch (IOException ex) {
-//         ex.printStackTrace();
+     File file = new File("student_data.txt");
+     try {
+         if (file.createNewFile()) {
+             System.out.println("student_data.txt created");
+         } else {
+             System.out.println("student_data.txt already exists");
+         }
+     } catch (IOException ex) {
+         ex.printStackTrace();
      }
     }
 }
