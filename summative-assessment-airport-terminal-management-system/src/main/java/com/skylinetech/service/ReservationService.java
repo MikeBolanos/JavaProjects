66938c8802241d997fa6147df63aae08d41c148c
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-30 13:39:31 -0500

    Create ReservationService.java
    Coding Reservation Service to handle reservations.

---
+++ summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/service/ReservationService.java
@@ -0,0 +1,16 @@
+package com.skylinetech.service;
+
+import com.skylinetech.model.*;
+import com.skylinetech.utils.Utils;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ReservationService {
+    // Map: (key)Flight number (value)List of passengers
+    private Map<String, List<Passenger>> reservations = new HashMap<>();
+
+
+
+}
