52e530c592d0d89ce5cb9c59455aa04ec1d188ad
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-30 13:49:56 -0500

    Update ReservationSystem.java
    Renamed the Service file to ReservationSystem as per the instructions.

--- summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/service/ReservationSystem.java
+++ summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/service/ReservationSystem.java
@@ -1,13 +1,12 @@
 package com.skylinetech.service;
 
 import com.skylinetech.model.*;
-import com.skylinetech.utils.Utils;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class ReservationService {
+public class ReservationSystem {
     // Map: (key)Flight number (value)List of passengers
     private Map<String, List<Passenger>> reservations = new HashMap<>();
 
