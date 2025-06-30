e7a6211ddb3577c9338a36eb51eb89f56873a625
MikeXander42 <michaelxbolanos@gmail.com>
2025-06-29 21:23:58 -0500

    Initializing classes
    Coding sub classes for aircrafts.

--- summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/model/CommercialAircraft.java
+++ summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/model/CommercialAircraft.java
@@ -1,4 +1,14 @@
 package com.skylinetech.model;
 
-public class CommercialAircraft {
+public class CommercialAircraft extends Aircraft {
+    private String airlineName;
+
+    public CommercialAircraft(String model, int capacity, double fuelCapacity, String airlineName) {
+        super(model, capacity, fuelCapacity);
+        this.airlineName = airlineName;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
 }
--- summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/model/PrivateJet.java
+++ summative-assessment-airport-terminal-management-system/src/main/java/com/skylinetech/model/PrivateJet.java
@@ -1,4 +1,21 @@
 package com.skylinetech.model;
 
-public class PrivateJet {
+public class PrivateJet extends Aircraft {
+    private boolean hasLuxuryService;
+    private int maxSpeed; // in km/hr
+
+
+    public PrivateJet(String model, int capacity, double fuelCapacity, boolean hasLuxuryService, int maxSpeed) {
+        super(model, capacity, fuelCapacity);
+        this.hasLuxuryService = hasLuxuryService;
+        this.maxSpeed = maxSpeed;
+    }
+
+    public boolean isHasLuxuryService() {
+        return hasLuxuryService;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
 }
