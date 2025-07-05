0ef2d80eafce3de6e6c437154d8d4eab7e01328f
MikeXander42 <michaelxbolanos@gmail.com>
2025-07-05 17:25:25 -0500

    Update AddPassengerCommand.java
    Updating command to add passenger.

--- summative-assessment-airport-terminal-management-system/src/main/java/com/airport/domain/command/AddPassengerCommand.java
+++ summative-assessment-airport-terminal-management-system/src/main/java/com/airport/domain/command/AddPassengerCommand.java
@@ -2,23 +2,21 @@ package com.airport.domain.command;
 
 import com.airport.domain.model.Flight;
 import com.airport.domain.model.Passenger;
+import com.airport.domain.repository.FlightRepository;
 import com.airport.domain.reservation.ReservationSystem;
 
 public class AddPassengerCommand implements Command {
-    private ReservationSystem system;
-    private Flight flight;
-    private Passenger passenger;
+    private final ReservationSystem reservationSystem;
+    private final FlightRepository flightRepository;
 
-    public AddPassengerCommand(ReservationSystem system, Flight flight, Passenger passenger) {
-        this.system = system;
-        this.flight = flight;
-        this.passenger = passenger;
+    public AddPassengerCommand(ReservationSystem reservationSystem, FlightRepository flightRepository) {
+        this.reservationSystem = reservationSystem;
+        this.flightRepository = flightRepository;
     }
 
     @Override
     public void execute() {
-        system.addReservation(flight, passenger);
-    }
+        
 
 
 
