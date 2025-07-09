0385b06ff70a425bf165478084b90af34780cbee
MikeXander42 <michaelxbolanos@gmail.com>
2025-07-09 15:51:05 -0500

    Created project
    Created summative assessment project with Spring Boot. Established package structure.

---
+++ summative-assessment-inventory-manager/src/test/java/com/classicbit/inventory/InventoryManagerAppTests.java
@@ -0,0 +1,13 @@
+package com.classicbit.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
