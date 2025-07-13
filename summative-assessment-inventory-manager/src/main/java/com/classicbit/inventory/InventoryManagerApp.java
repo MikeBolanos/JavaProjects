56c110a50a6e7fdd01355d58b7dbc37453c0f7f8
MikeXander42 <michaelxbolanos@gmail.com>
2025-07-13 17:29:33 -0500

    Created InventoryManagerApp.java
    Coded the main application.

--- summative-assessment-inventory-manager/src/main/java/com/classicbit/inventory/InventoryManagerApp.java
+++ summative-assessment-inventory-manager/src/main/java/com/classicbit/inventory/InventoryManagerApp.java
@@ -1,13 +1,43 @@
 package com.classicbit.inventory;
 
+import com.classicbit.inventory.view.AdminInventory;
+import com.classicbit.inventory.view.Kiosk;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class InventoryManagerApp {
+@SpringBootApplication // Enables component scanning and auto-configuration
+public class InventoryManagerApp implements CommandLineRunner {
+
+	// Inject Admin interface to manage inventory
+	@Autowired
+	private AdminInventory adminInventory;
+
+	// Inject the Kiosk interface for customers to shop
+	@Autowired
+	private Kiosk kiosk;
+
+	/**
+	 * Read the startup mode from application.properties:
+	 * If not specified, default to "kiosk"
+	 * Example line in properties: classicbit.mode=admin
+	 */
+
+	@Value("${classicbit.mode:kiosk}")
+	private String mode;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagerApp.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		if (mode.equalsIgnoreCase("admin")) {
+			adminInventory.run();
+		} else {
+			kiosk.run();
+		}
+	}
 }
