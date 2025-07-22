33ce38ef312fe4cb3d98fb34aacbcc1241f75f6b
MikeXander42 <michaelxbolanos@gmail.com>
2025-07-22 10:51:57 -0500

    Created Spring Project
    Created new Spring project for summative for databases in Java.

---
+++ summative-assessment-java-databases/src/test/java/com/example/jdbc/summative/SummativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jdbc.summative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
